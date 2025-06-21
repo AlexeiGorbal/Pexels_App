a7e31b8a73963a52e81434ec23857240a1882119
lemon <lemon.juice9341@gmail.com>
2025-06-21 22:45:51 +0300

    added a cache

--- app/src/main/java/com/example/pexelsapp/web/NetworkModule.kt
+++ app/src/main/java/com/example/pexelsapp/web/NetworkModule.kt
@@ -1,10 +1,13 @@
 package com.example.pexelsapp.web
 
+import android.content.Context
 import com.example.pexelsapp.domain.remote.PexelsAppApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -23,7 +26,9 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(@ApplicationContext context:Context): Retrofit {
+        val cacheSize = (5 * 1024 * 1024).toLong()
+        val myCache = Cache(context.cacheDir, cacheSize)
         val logger = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
@@ -31,6 +36,7 @@ class NetworkModule {
             .baseUrl("https://api.pexels.com/v1/")
             .client(
                 OkHttpClient.Builder()
+                    .cache(myCache)
                     .addInterceptor(ApiKeyInterceptor())
                     .addInterceptor(logger)
                     .build()
